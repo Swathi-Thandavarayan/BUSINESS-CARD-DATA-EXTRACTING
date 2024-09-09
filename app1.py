d3f1fba9f4ba26cf0f16b7db8cf37e140682457d
Swathi Thandavarayan <159911444+Swathi-Thandavarayan@users.noreply.github.com>
2024-09-09 14:37:33 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -6,5 +6,22 @@ reader = easyocr.Reader(['en'])
 image_path = '1.png'
 result = reader.readtext(image_path)
 for detection in result:
-    print(detection[1]) 
-st.write(detection)
+    st.write(detection[1]) 
+
+st.write('detection')
+
+
+uploaded_files = st.file_uploader(
+    "Choose a file", accept_multiple_files=True
+)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    st.write(bytes_data)
+
+    if uploaded_file is not None:
+    # Open the image using PIL
+    image = Image.open(uploaded_file)
+    
+    # Display the image
+    st.image(image, caption="Uploaded Image", use_column_width=True)
